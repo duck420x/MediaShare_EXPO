3ca62c5e51ef5c506dff6a3ea5e251cc75e3a393
Oliver <oliver.boros.nl@gmail.com>
2023-06-22 10:40:54 +0200

    home

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ include "./loginCheck.php";
 </head>
 
 <body>
-	<nav class="navbar has-background-light box p-1">
+	<!-- NAV-BAR -->
+	<nav class="navbar has-background-light p-1">
 		<div class="navbar-brand">
 			<logo class="nav-item logo">
 				<!-- //-- Mogelijkheid voor logo --// -->
@@ -37,19 +38,159 @@ include "./loginCheck.php";
 			</div>
 		</div>
 	</nav>
-	<section class="hero is-link is-fullheight-with-navbar">
-		<div class="hero-body">
-			<p class="title">
-				<img src="./images/NoBgLogoRemake.svg" alt="Logo" />
-			</p>
-			<div class="subtitle buttons">
-				<button class="button is-primary"><a href="./login.html">Login</a></button>
-				<button class="button is-warning"><a href="./register.html">Register</a></button>
-				<button class="button is-danger"><a href="./logout.php">Logout</a></button>
+	<!-- MAIN -->
+	<!-- SECTION -->
+	<section class="hero is-primary is-xs box">
+		<div class="hero-head">
+			<nav class="navbar">
+				<div class="container">
+					<div class="navbar-brand">
+						<a class="navbar-item">
+							<input class="input tekst-center" type="text" placeholder="Search" style="width: 66vw" />
+						</a>
+					</div>
+					<div id="navbarMenuHeroA" class="navbar-menu">
+						<div class="navbar-end">
+							<span class="navbar-item">
+								<a class="button is-primary is-inverted" href="./upload.php">
+									<span>New Post</span>
+								</a>
+							</span>
+							<a class="navbar-item"> Activity </a>
+							<a class="navbar-item">
+								<i class="fa-solid fa-user fa-xl"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<div class="hero-body" style="padding-top: 2%; margin: 0% 12%">
+			<!-- BOX-BEGIN -->
+			<div class="container has-text-centered">
+				<?php
+				include_once "./read.php";
+
+				// Retrieve the current user's ID from the session or any other source
+				$userId = $_SESSION['id']; // Assuming you have stored the user's ID in the session
+
+				// Retrieve and display the posts
+				$posts = getPosts($userId);
+
+				foreach ($posts as $post) {
+					$postId = $post['id'];
+					$postUserId = $post['User_ID'];
+					$postTitle = $post['Titel'];
+					$postText = $post['Text'];
+					$postImage = $post['Media'];
+					$postLikes = $post['Likes'];
+
+					// Render the post HTML snippet for each post
+					echo '<div class="container has-text-centered">
+            <div class="box mt-6">
+                <div class="columns">
+                    <div class="column is-11 content-image">
+                        <img src="' . $postImage . '" class="image" alt="Post" />
+                    </div>
+                    <div class="column is-1">
+                        <div class="columns is-vcentered is-multiline">
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-solid fa-arrow-up fa-xl"></i>
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-solid fa-arrow-down fa-xl"></i>
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-regular fa-comment fa-xl"></i>
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-sharp fa-regular fa-hashtag fa-xl"></i>
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-solid fa-user fa-xl"></i>
+                                </div>
+                            </div>
+                            <div class="column is-12">
+                                <div class="notification">
+                                    <i class="fa-solid fa-ellipsis fa-xl"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>';
+				}
+				?>
+
 			</div>
+
+			<!-- <div class="container has-text-centered">
+				<div class="box mt-6">
+					<div class="columns">
+						<div class="column is-11 content-image">
+							<img src="" class="image" alt="Post" />
+						</div>
+						<div class="column is-1">
+							<div class="columns is-vcentered is-multiline">
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-solid fa-arrow-up fa-xl"></i>
+									</div>
+								</div>
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-solid fa-arrow-down fa-xl"></i>
+									</div>
+								</div>
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-regular fa-comment fa-xl"></i>
+									</div>
+								</div>
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-sharp fa-regular fa-hashtag fa-xl"></i>
+									</div>
+								</div>
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-solid fa-user fa-xl"></i>
+									</div>
+								</div>
+								<div class="column is-12">
+									<div class="notification">
+										<i class="fa-solid fa-ellipsis fa-xl"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> -->
+
 		</div>
 	</section>
+	<footer class="footer footer-top">
+		<div class="content has-text-centered">
+			<p>
+				This project is made by <strong>Oliver Boros</strong> and <strong>Stefano Ceoldo</strong>.
+				This code is copyrighted. The website is supported by
+				<a href="https://glr.nl">Grafisch Lyceum Rotterdam</a>.
+			</p>
+		</div>
+	</footer>
 </body>
+<script src="https://kit.fontawesome.com/1f8796d395.js" crossorigin="anonymous"></script>
 <script src="./js/BulmaHamburgToggle.js"></script>
 
 </html>
